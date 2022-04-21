4cb71295831a555a190760174fab0b5aed82df72
adachi_440 <tomoki1999314@gmail.com>
2022-04-21 20:48:23 +0900

    eslintの実行

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "google",
+    'env': {
+        'browser': true,
+        'es2021': true,
+      },
+      'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
         'next/core-web-vitals',
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+        'google',
+      ],
+      'parser': '@typescript-eslint/parser',
+      'parserOptions': {
+        'ecmaFeatures': {
+          'jsx': true,
         },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+      },
+      'plugins': [
+        'react',
+        'react-hooks',
+        '@typescript-eslint',
+      ],
+      'rules': {
+        'semi': ['error', 'never'],
+        'require-jsdoc': ['off'],
+        'spaced-comment': ['off'],
+      },
 }
